4f6d48a6a70e6c06aa5281a089634a30c4b4965d
Kazooooo <cest93@gmail.com>
2017-09-09 17:36:30 +0900

    コメントの削除、Lintエラー対応、interface調整

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -12,51 +12,31 @@ import './Hello.css';
 
 interface HelloProps extends RouteComponentProps<StoreState> {
   name: string;
-  enthusiasmLevel?: number;
-  onIncrement?: () => void;
-  onDecrement?: () => void;
+  enthusiasmLevel: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
 }
 
-// @connect((state: StoreState) => ({
-//   name: state.enthusiasm.languageName,
-//   enthusiasmLevel: state.enthusiasm.enthusiasmLevel,
-// }), { onIncrement, onDecrement })
-class Hello extends React.Component<HelloProps, any> {
+class Hello extends React.Component<HelloProps, null> {
   constructor(props) {
     super(props);
-    console.log(props);
-  }
-
-  // ({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: HelloProps) {
-  // if (enthusiasmLevel <= 0) {
-  //   throw new Error('You could be a little more enthusiastic. :D');
-  // }
-  public componentWillReceiveProps(nextProps) {
-    console.log(nextProps);
   }
 
   public render() {
-    const {
-      enthusiasmLevel,
-      onDecrement,
-      onIncrement,
-      name,
-    } = this.props;
+    const { props } = this;
 
-    console.log(enthusiasmLevel);
-
-    if (enthusiasmLevel <= 0) {
+    if (props.enthusiasmLevel <= 0) {
       throw new Error('You could be a little more enthusiastic. :D');
     }
 
     return (
       <div className="hello">
         <div className="greeting">
-          Hello {`${name} ${getExclamationMarks(enthusiasmLevel)}`}
+          Hello {`${name} ${getExclamationMarks(props.enthusiasmLevel)}`}
         </div>
         <div>
-          <button onClick={onDecrement}>-</button>
-          <button onClick={onIncrement}>+</button>
+          <button onClick={props.onDecrement}>-</button>
+          <button onClick={props.onIncrement}>+</button>
         </div>
       </div>
     );
@@ -64,13 +44,10 @@ class Hello extends React.Component<HelloProps, any> {
 }
 
 export default connect(
-  (state) => {
-    console.log(state);
-    return {
-      name: state.enthusiasm.languageName,
-      enthusiasmLevel: state.enthusiasm.enthusiasmLevel,
-    };
-  }, { onIncrement, onDecrement },
+  (state) => ({
+    name: state.enthusiasm.languageName,
+    enthusiasmLevel: state.enthusiasm.enthusiasmLevel,
+  }), { onIncrement, onDecrement },
 )(Hello as typeof React.Component);
 
 // helpers
